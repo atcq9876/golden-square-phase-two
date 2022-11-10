d28316d0cdd01ab88491e7978a0abf72e15c68ee
Andy <ajscottlewis@gmail.com>
2022-11-10 17:27:36 +0000

    Add good and bad counters

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,13 +1,24 @@
 class GrammarStats
   def initialize
-    # ...
+    @good = 0
+    @bad = 0
   end
 
   def check(text) # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
-    return false if text.empty? 
-    text[0].upcase == text[0] and ['!','?','.'].include?(text[-1]) ? true : false
+    if text.empty? 
+      @bad += 1
+      return false
+    end
+
+    if text[0].upcase == text[0] and ['!','?','.'].include?(text[-1])
+      @good += 1
+      true
+    else
+      @bad += 1
+      false
+    end
   end
 
   def percentage_good
