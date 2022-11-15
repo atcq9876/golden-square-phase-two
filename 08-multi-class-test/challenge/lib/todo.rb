dc6909389627a63801276696b3439d7b699b5537
Andy <ajscottlewis@gmail.com>
2022-11-15 13:20:10 +0000

    Write all integration tests

--- 08-multi-class-test/challenge/lib/todo.rb
+++ 08-multi-class-test/challenge/lib/todo.rb
@@ -1,6 +1,7 @@
 class Todo
   def initialize(task) # task is a string
-    # ...
+    @task = task
+    @complete = []
   end
 
   def task
@@ -10,11 +11,12 @@ class Todo
   def mark_done!
     # Marks the todo as done
     # Returns nothing
+    @complete << @task
   end
 
   def done?
     # Returns true if the task is done
     # Otherwise, false
+    @complete.include?(@task)
   end
 end
-
--- 08-multi-class-test/challenge/lib/todo_list.rb
+++ 08-multi-class-test/challenge/lib/todo_list.rb
@@ -1,21 +1,52 @@
 class TodoList
   def initialize
+    @list = []
+    @complete = []
   end
 
   def add(todo) # todo is an instance of Todo
     # Returns nothing
+    @list << todo
   end
 
   def incomplete
     # Returns all non-done todos
+    update_list
+    @list.empty? ? (raise "No incomplete todos") : @list
   end
 
   def complete
     # Returns all complete todos
+    update_list
+    @complete.empty? ? (raise "No completed todos") : @complete
   end
 
   def give_up!
     # Marks all todos as complete
+    update_list
+    if @list.empty?
+      raise "All todos already done!"
+    else
+      "No more todos!"
+    end
   end
 end
 
+def update_list
+  @list.each do |todo|
+    if todo.done? == true
+      @complete << todo
+      @list.delete(todo)
+    end
+  end
+end
+
+=begin
+write a method for updating todo list and complete list:
+@list.each do |todo|
+  if todo.done? == true
+    @complete << todo
+    @list.delete(todo)
+  end
+end
+=end
