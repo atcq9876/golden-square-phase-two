c75f3257348163b2e3834f1c465e52ebc027cc76
Andy <ajscottlewis@gmail.com>
2022-11-16 16:58:37 +0000

    Add integration and unit tests for Diary and DiaryEntry

--- 09-multi-class-design/exercise-challenge/lib/diary.rb
+++ 09-multi-class-design/exercise-challenge/lib/diary.rb
@@ -3,48 +3,31 @@ class Diary
     @entries = []
   end
 
-  def add(entry) # entry is an instance of DiaryEntry
-    # Returns nothing
+  def add(entry)
     @entries << entry
   end
 
-  def all
-    # Returns a list of instances of DiaryEntry
-    raise "No diary entries added" if @entries.empty?
+  def read_all
     @entries
   end
 
-  def count_words
-    # Returns the number of words in all diary entries
-    # HINT: This method should make use of the `count_words` method on DiaryEntry.
-    raise "No diary entries added" if @entries.empty?
-    sum = 0
-    @entries.each do |entry| 
-      sum += entry.count_words
+  def read_best_entry(wpm, minutes)
+    "Six seven eight"
+    words_to_read = wpm * minutes
+    current_difference = words_to_read
+    best_entry = ""
+    @entries.each do |entry|
+      if (words_to_read - entry.contents.split.length) < 0
+        next
+      elsif (words_to_read - entry.contents.split.length) <= current_difference
+        best_entry = entry.contents
+      end
     end
-    sum
-  end
-
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
-    raise "wpm must be > 0" if wpm <= 0
-    count_words.to_f / wpm
-  end
-
-  def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
-    raise "wpm must be > 0" if wpm <= 0
-    raise "No diary entries added" if @entries.empty?
-    possibilities = @entries.select{|entry| entry.reading_time(wpm) <= minutes}
-    raise "They are all too long" if possibilities.empty?
-    possibilities.sort_by{|entry| entry.reading_time(wpm)}[0]
+    if best_entry == ""
+      raise "You don't have enough time to read an entry"
+    else
+      best_entry
+    end
   end
 end
+
--- 09-multi-class-design/exercise-challenge/spec/diary_integration_spec.rb
+++ 09-multi-class-design/exercise-challenge/spec/diary_integration_spec.rb
@@ -1,108 +1,50 @@
-require 'diary_entry.rb'
-require 'diary.rb'
+require 'diary'
+require 'diary_entry'
 
-RSpec.describe "integration" do
-  describe "all method" do
-    context "one diary entry" do
-      it "returns all of one diary entry" do
-        entry_1 = DiaryEntry.new("Title 1", "Contents 1")
-        diary = Diary.new
-        diary.add(entry_1)
-        expect(diary.all).to eq [entry_1]
-      end
-    end
-    context "two diary entries" do
-      it "returns all of 2 diary entries" do
-        entry_1 = DiaryEntry.new("Title 1", "Contents 1")
-        entry_2 = DiaryEntry.new("Title 2", "Contents 2")
-        diary = Diary.new
-        diary.add(entry_1)
-        diary.add(entry_2)
-        expect(diary.all).to eq [entry_1, entry_2]
-      end
-    end
-  end
-
-  describe "count_words" do
-    context "one diary entry" do 
-      it "counts number of words in one diary entry" do
-          diary = Diary.new
-          entry_1 = DiaryEntry.new("Title 1", "one two three")
-          diary.add(entry_1)
-          expect(diary.count_words).to eq 3
-      end
-    end
-    context "two diary entries" do
-      it "counts number of words in two diary entries" do
-          diary = Diary.new
-          entry_1 = DiaryEntry.new("Title 1", "one two three")
-          entry_2 = DiaryEntry.new("Title 2", "four five six")
-          diary.add(entry_1)
-          diary.add(entry_2)
-          expect(diary.count_words).to eq 6
-      end
-    end
-  end     
-
-  describe "reading_time" do
-    context "one diary entry" do  
-      it "returns the reading time for all of one entries" do
-        diary = Diary.new
-        entry_1 = DiaryEntry.new("Title 1", "one two three")
-        diary.add(entry_1)
-        expect(diary.reading_time(3)).to eq 1
-      end
-    end
-    context "two diary entries" do
-      it "returns the total reading time for all two entries" do
-        diary = Diary.new
-        entry_1 = DiaryEntry.new("Title 1", "one two three")
-        entry_2 = DiaryEntry.new("Title 2", "four five six")
-        diary.add(entry_1)
-        diary.add(entry_2)
-        expect(diary.reading_time(3)).to eq 2
-      end
-    end
-  end
-  context "reading_time: one diary entry but wpm <= 0" do
-    it "fails" do
+describe 'integration' do
+  describe '#add' do
+    it 'adds diary entries to diary' do
+      entry_1 = DiaryEntry.new("Title 1", "One")
+      entry_2 = DiaryEntry.new("Title 2", "Two")
       diary = Diary.new
-      entry_1 = DiaryEntry.new("Title 1", "one two three")
       diary.add(entry_1)
-      expect { diary.reading_time(0) }.to raise_error "wpm must be > 0"
+      expect(diary.add(entry_2)).to eq [entry_1, entry_2]
     end
   end
 
-  describe "find_best_entry_for_reading_time" do
-    context "two diary entries" do
-      it "returns the best entry given wpm" do
+  describe '#read_all' do
+    it 'returns all diary entries' do
+      entry_1 = DiaryEntry.new("Title 1", "One")
+      entry_2 = DiaryEntry.new("Title 2", "Two")
+      diary = Diary.new
+      diary.add(entry_1)
+      diary.add(entry_2)
+      expect(diary.read_all).to eq [entry_1, entry_2]
+    end
+  end
+
+  describe "#read_best_entry" do
+    context "with two entries, only one short enough to read" do
+      it "returns the entry that the user can read in the given time" do
+        entry_1 = DiaryEntry.new("Title 1", "One two three four five")
+        entry_2 = DiaryEntry.new("Title 2", "Six seven eight")
         diary = Diary.new
-        entry_1 = DiaryEntry.new("Title 1", "one two")
-        entry_2 = DiaryEntry.new("Title 2", "three four five six")
         diary.add(entry_1)
         diary.add(entry_2)
-        expect(diary.find_best_entry_for_reading_time(2,1)).to eq entry_1
+        expect(diary.read_best_entry(3, 1)).to eq "Six seven eight"
       end
     end
-    context "two diary entries but both too long to read in given time" do
-      it "raises error" do
+
+    context "with two entries both being too long" do
+      it "fails" do
+        entry_1 = DiaryEntry.new("Title 1", "One two three four five")
+        entry_2 = DiaryEntry.new("Title 2", "Six seven eight")
         diary = Diary.new
-        entry_1 = DiaryEntry.new("Title 1", "one two")
-        entry_2 = DiaryEntry.new("Title 2", "three four five six")
         diary.add(entry_1)
         diary.add(entry_2)
-        expect{ diary.find_best_entry_for_reading_time(1,1) }.to raise_error "They are all too long"
+        expect { diary.read_best_entry(2, 1) }.to raise_error "You don't have enough time to read an entry"
       end
     end
-    context "two diary entries and wpm of 0" do
-      it "raises error" do
-        diary = Diary.new
-        entry_1 = DiaryEntry.new("Title 1", "one two")
-        entry_2 = DiaryEntry.new("Title 2", "three four five six")
-        diary.add(entry_1)
-        diary.add(entry_2)
-        expect{ diary.find_best_entry_for_reading_time(0,1) }.to raise_error "wpm must be > 0"
-      end
-    end
-  end  
+  end
 end
+
